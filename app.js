1034be67300bee5c2f161e1454480e7ea5b5d1e1
donianasr-ship-it <donia.nasr@donorionefano.edu.it>
2026-05-26 09:36:21 +0200

    Add files via upload

---
+++ app.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const app = express();
+const port = 8080;
+
+// Respond to GET request on the root route
+app.get('/', (req, res) => {
+  res.send('GET request to the homepage');
+});
+
+// Respond to POST request on the root route
+app.post('/', (req, res) => {
+  res.send('POST request to the homepage');
+});
+
+// Respond to GET request on the /about route
+app.get('/about', (req, res) => {
+  res.send('About page');
+});
+
+// Gestione dell'errore 404 (Valido per Express 5)
+app.use((req, res) => {
+  res.status(404).send('404 - Page not found');
+});
+
+// Start the server
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
